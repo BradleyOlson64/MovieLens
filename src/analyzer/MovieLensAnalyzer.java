2a5e0c40c2ec166ac3f9a0ef76ecf806bedcce46
Thavy Thach <thavythach@gmail.com>
2018-11-24 15:20:24 -0800

    comments added.

--- src/analyzer/MovieLensAnalyzer.java
+++ src/analyzer/MovieLensAnalyzer.java
@@ -34,32 +34,37 @@ public class MovieLensAnalyzer {
 		
 		
 		// FILL IN THE REST OF YOUR PROGRAM
+		
+		// TODO: these will be command-line arguments
 		String ratingsString = "./src/ml-latest-small/ratings.csv";
 		String moviesString = "./src/ml-latest-small/movies.csv";
 		
+		// utilize ETL(EXTRACT|TRANSFORM|LOAD) process
 		DataLoader movieLensDataLoader = new DataLoader();
-		
 		movieLensDataLoader.loadData(moviesString, ratingsString);
-		
-		
 		HashMap<Integer, Movie> movies = (HashMap) movieLensDataLoader.getMovies();
 		HashMap<Integer, Reviewer> ratings = (HashMap) movieLensDataLoader.getReviewers();
 		
-		// TODO: turn into StringBuilder object
+		
+		// welcome the user output
 		System.out.println("=========Welcome to MovieLens Analyzer=========");
 		
+		// TODO: this needs to be command-line arguments
 		System.out.println("The Files being analyzed are:\n" + ratingsString + "\n" + moviesString + "\n");
 		System.out.println("There are 3 choices for defining adjacency");
         
-		// TODO: scanner sucks - do stuff with try catch blocks nerd
-		Scanner sc = new Scanner(System.in);  
-        System.out.println("[Option 1] u and v are adjacent if both movies have 50 reviewers that gave the same rating.");
+		// initialize input system for user
+		Scanner sc = new Scanner(System.in);
+		
+		// display options to pick how movies are related to each other
+        System.out.println("[Option 1] u and v are adjacent if both movies have at least 50 reviewers that gave the same rating.");
         System.out.println("[Option 2] u and v are adjacent if both movies were made in the same year (regardless of rating).");
         System.out.println("[Option 3] u and v are adjacent if both movies contain the same string (Exploring the Graph Extra Credit).");
         
+        // initialize option picker for user to determine how movies are related to each other
         System.out.println("\nChoose an Option to build the graph (1-3):");
-        
         int adjOption = 4;
+        
         try {
         		adjOption = sc.nextInt();
         } catch (InputMismatchException e) {
@@ -67,25 +72,25 @@ public class MovieLensAnalyzer {
         		System.exit(0);
         }
        
-        
+        // create populated graph based on option picked by user
         Graph<Integer> tmpGraph = new Graph<Integer>();
-        switch (adjOption) {
-        	case 1:
-        		tmpGraph  = option_one(movies, ratings);
-        		break;
-        	case 2: 
-        		tmpGraph = option_two(movies, ratings);
-        		break;
-        	case 3:
-        		tmpGraph = option_three(movies, ratings);
-        		break;
-        	default:
-        		System.out.println("You've picked a wrong option. Program is exiting.");
-        		System.exit(0);
-        		break;
-
+	        switch (adjOption) {
+	        	case 1:
+	        		tmpGraph  = optionFiftyReviewersRating(movies, ratings);
+	        		break;
+	        	case 2: 
+	        		tmpGraph = optionSameYear(movies, ratings);
+	        		break;
+	        	case 3:
+	        		tmpGraph = optionSearchString(movies);
+	        		break;
+	        	default:
+	        		System.out.println("You've picked a wrong option. Program is exiting.");
+	        		System.exit(0);
+	        		break;
         }
 
+	    // initialize continuous option picker to find stats, node info, and shortest path(s) in the graph.
         int exploreOption = 4;
         
         while (true) {
@@ -120,77 +125,67 @@ public class MovieLensAnalyzer {
         }    
     	}
 	
-	@SuppressWarnings("null")
-	public static Graph<Integer> option_one(HashMap<Integer, Movie> movies, HashMap<Integer, Reviewer> reviewers) {
-//		System.out.println("Entering option one"); // TODO: remove
+	/**
+	 * Fifty Reviewers Rating Option populates a graph based on at least 50 users who gave the same rating to movies (ex. if movie_1 had 57 ratings for 5.0 and movie_2 had 87 ratings for 5.0).
+	 * @param movies dataset of movies
+	 * @param reviewers dataset of reviewers
+	 * @return populated graph based on at least 50 users who gave the same rating to movies
+	 */
+	public static Graph<Integer> optionFiftyReviewersRating(HashMap<Integer, Movie> movies, HashMap<Integer, Reviewer> reviewers) {
 		
 		// Create an empty Graph of Movies
 		Graph<Integer> moviesGraph = new Graph<Integer>();
 		
 		// Populate graph with the movies dataset
 		for ( Integer tmp : movies.keySet()) {
-			// System.out.println("Adding Vertex @ DEBUG [1]: " + tmp); // Debug: Vertices are added.
 			moviesGraph.addVertex(tmp);
-			// System.out.println("Contains Vertex @ DEBUG[2]: " +  moviesGraph.containsNode(tmp)); // Debug: check if vertices have been aded.
 		}
 		
 		// Create a 2D Array of Integers that tells whether each movie got over a certain number (THRESHOLD) for each type of review. 
-		int[][] alpha = new int[moviesGraph.numVertices()][RATINGS_ARRAY_SIZE];
-//		System.out.println("numVertices() @ DEBUG [3]: " + moviesGraph.numVertices());
-//		System.out.println("RATINGS_ARRAY_SIZE @ DEBUG [4]: " + RATINGS_ARRAY_SIZE); // 2D[x][0] = 0.0, 2D[x][1] = 0.5, ..., etc.
+		int[][] thresholdHolder = new int[moviesGraph.numVertices()][RATINGS_ARRAY_SIZE];
 		
-		Integer[] reviewerIds = reviewers.keySet().toArray(new Integer[reviewers.keySet().size()]);		
-//		System.out.println("intRatings.length @ DEBUG[5]: " + reviewerIds.length);
-		
-		
+		// get movies and get reviewers dataset
+		Integer[] reviewerIds = reviewers.keySet().toArray(new Integer[reviewers.keySet().size()]);	
 		Integer[] moviesIds = moviesGraph.getVertices().toArray(new Integer[moviesGraph.getVertices().size()]);
 		
 		
-		// fill the alpha array with ratings
+		// fill the thresholdHolder array with ratings increments
 		Double tmp = 0.0;
 		for ( int i=0; i < moviesIds.length; i++) {
 			for (int j=0; j < reviewerIds.length; j++) {
 				tmp = reviewers.get(reviewerIds[j]).getRatings().get(moviesIds[i]);
 				if (tmp != null) {
-//					System.out.println(moviesIds[i]);
-//					System.out.println("Other: " + tmp);
-					alpha[i][(int) (tmp * 2)] += 1;
+					thresholdHolder[i][(int) (tmp * 2)] += 1;
 				}
-//				break;
 			}
 		}
 		
-		
+		// populate graph based on threshholdHolder's values which is dictated by THRESHOLD value
 		for (int i=0; i < moviesIds.length; i++) {
-//			System.out.println("[movie id]= "+ moviesIds[i] + " => " + Arrays.toString(alpha[i]));
-			for (int j=0; j < alpha[i].length; j++) {
+			for (int j=0; j < thresholdHolder[i].length; j++) {
 								
-				if (alpha[i][j] >= THRESHOLD) {
-//					System.out.println(alpha[i][j]);
-					for (int k=0; k < alpha.length; k++) {
-						if (alpha[k][j] >= THRESHOLD && k != i && !moviesGraph.edgeExists(moviesIds[i], moviesIds[k]) && !	moviesGraph.edgeExists(moviesIds[k], moviesIds[i])) {
-//							System.out.println("[ids i and k]: = "+ moviesIds[i] + ", "+ moviesIds[k]);
-//							System.out.println("[ids i and k]: = "+ moviesIds[k] + ", "+ moviesIds[i]);
-//							System.out.format("\ti:[%d], j:[%d] = [%d]\n",i, j, alpha[i][j]);
-//							System.out.format("\tk:[%d], j:[%d] = [%d]\n",i, j, alpha[k][j]);
-//							System.out.println("\n");
+				if (thresholdHolder[i][j] >= THRESHOLD) {
+					for (int k=0; k < thresholdHolder.length; k++) {
+						if (thresholdHolder[k][j] >= THRESHOLD && k != i && !moviesGraph.edgeExists(moviesIds[i], moviesIds[k]) && !	moviesGraph.edgeExists(moviesIds[k], moviesIds[i])) {
+
 							moviesGraph.addEdge(moviesIds[i], moviesIds[k]);
 							moviesGraph.addEdge(moviesIds[k], moviesIds[i]);
 						}
 					}
 				}
 			}
-//			if (i==50) break;
 		}
-//		System.out.println(Arrays.deepToString(alpha));
-
-
-		
-		
 		return moviesGraph;
 	
 	}
-	public static Graph<Integer> option_two(HashMap<Integer, Movie> movies, HashMap<Integer, Reviewer> reviewers) { 
+	
+	/**
+	 * Same Year Option populates a movie graph based on the year it was created (ex. all movies made in 2006 will be neighbors with each other).
+	 * @param movies dataset of movies
+	 * @param reviewers dataset of reviewers
+	 * @return a populated graph based on the year the movie was created.
+	 */
+	public static Graph<Integer> optionSameYear(HashMap<Integer, Movie> movies, HashMap<Integer, Reviewer> reviewers) { 
 		
 		// Create an empty Graph of Movies
 		Graph<Integer> moviesGraph = new Graph<Integer>();
@@ -200,8 +195,10 @@ public class MovieLensAnalyzer {
 			moviesGraph.addVertex(tmp);
 		}
 		
+		// Get movies dataset
 		Integer[] moviesIds = moviesGraph.getVertices().toArray(new Integer[moviesGraph.getVertices().size()]);
 
+		// populate graph with edges based on year
 		int movieX = 0;
 		int movieY = 0;
 		for (int i=0; i < moviesIds.length; i++) {
@@ -210,58 +207,47 @@ public class MovieLensAnalyzer {
 			for (int j=0; j < moviesIds.length; j++) {
 				movieY = movies.get(moviesIds[j]).getYear();
 				if ( movieX == movieY && i != j && !moviesGraph.edgeExists(moviesIds[i], moviesIds[j])) {
-//					System.out.println("[ids i and k]: = "+ moviesIds[i] + ", "+ moviesIds[j]);
-//					System.out.println("\n");
 					moviesGraph.addEdge(moviesIds[i], moviesIds[j]);
 				}
 			}
 		}
-		
-	
-
-		
-		
 		return moviesGraph;
 	}
-	public static  Graph<Integer> option_three(HashMap<Integer, Movie> movies, HashMap<Integer, Reviewer> reviewers) { 
+	
+	/**
+	 * Search String option allows for a string input to populate a movie graph based on that string input (ex. casino gives back 1000 vertices but there exists only two edges).
+	 * @param movies dataset of movies
+	 * @return a populated graph of the intended string input.
+	 */
+	public static  Graph<Integer> optionSearchString(HashMap<Integer, Movie> movies) { 
 		
 		// Create an empty Graph of Movies
 		Graph<Integer> moviesGraph = new Graph<Integer>();
 		
-//		// Populate graph with the movies dataset
-//		for ( Integer tmp : movies.keySet()) {
-//			// System.out.println("Adding Vertex @ DEBUG [1]: " + tmp); // Debug: Vertices are added.
-//			// System.out.println("Contains Vertex @ DEBUG[2]: " +  moviesGraph.containsNode(tmp)); // Debug: check if vertices have been aded.
-//		}
-		
+		// Get movies dataset
 		Integer[] moviesIds = movies.keySet().toArray(new Integer[movies.keySet().size()]);
 		
 		// Get Keyword from user.
         System.out.println("Enter a new keyword to find all movies based on searched term:");
 		Scanner sc = new Scanner(System.in);
         String keyword = sc.nextLine();
-        
-
 		String titleX = "";
-		String titleY = "";
 		
+		// Storage for keyword matches
 		ArrayList<Integer> keywordMatchedMovies = new ArrayList<Integer>();
-		
-		// TODO: might need to populate all movies in the vertices to abide by option 2
 		
 		// create vertices based on keyword input
 		for (int i=0; i < moviesIds.length; i++) {
 			titleX = movies.get(moviesIds[i]).getTitle().toUpperCase();
 			
 			if ( titleX.contains(keyword.toUpperCase())) {
-//				System.out.println( titleX );
 				keywordMatchedMovies.add(moviesIds[i]);
 			} 
 			moviesGraph.addVertex(moviesIds[i]);
 
 		}
 		
-		// populate graph with edges 
+		// populate graph with edges based on keyword matches
 		for (int i=0; i < keywordMatchedMovies.size(); i++) {
 			for (int j=0; j < keywordMatchedMovies.size(); j++) {
 				if ( i != j) {
@@ -277,7 +263,7 @@ public class MovieLensAnalyzer {
 	
 	/**
 	 * Builds a string to print graph statistics about given graph data structure.
-	 * @param tmpGraph a graph
+	 * @param tmpGraph a populated graph
 	 */
 	public static void printGraphStats(Graph<Integer> tmpGraph) {
 		
@@ -308,6 +294,12 @@ public class MovieLensAnalyzer {
 		
 		System.out.println(sb.toString());
 	}
+	
+	/**
+	 * Displays information pertaining to any movieID node.
+	 * @param tmpGraph a populated graph
+	 * @param movies dataset of movies
+	 */
 	public static void printNodeInfo(Graph<Integer> tmpGraph, HashMap<Integer, Movie> movies) {
 		
 		StringBuilder sb = new StringBuilder("");
@@ -343,6 +335,11 @@ public class MovieLensAnalyzer {
 		System.out.println(sb.toString());
 		
 	}
+	
+	/**
+	 * Finds the shortest path between two nodes.
+	 * @param tmpGraph a populated graph
+	 */
 	public static void getShortestPath(Graph<Integer> tmpGraph) {
 		// TODO: pick only vertices that are within the graph
 		int[] shortestPath = GraphAlgorithms.dijkstrasAlgorithm(tmpGraph, 1000); // breaks at 2 and 50 and probably other values too.
